a216abc4348273cc4c9e56c31ab9497a11cfdaed
gCoreByte <21132799+gCoreByte@users.noreply.github.com>
2021-02-10 00:54:48 +0200

    .

--- cogs/newmember.py
+++ cogs/newmember.py
@@ -2,9 +2,22 @@ from discord.ext import commands
 
 
 def setup(bot):
-    bot.add_cog(Newmember(bot))
+    bot.add_cog(Firstjoin(bot))
 
 
-class Newmember(commands.Cog):
+class Firstjoin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    async def on_member_join(self, member):
+        # TODO
+
+        pass
+
+    async def on_guild_join(self, guild):
+        # TODO
+        pass
+
+    async def on_member_ban(self, guild, member):
+        # TODO
+        pass
